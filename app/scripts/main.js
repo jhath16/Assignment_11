e08039f2689a93aae61c167e26eacd2a148ce8e7
jhath16 <hathaway.justin@gmail.com>
2014-09-16 00:26:00 -0400

    POST and GET both work

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,37 @@
-console.log('The Iron Yard Rocks');
+var url = "http://tiny-pizza-server.herokuapp.com/collections/chirp";
+
+function renderTemplate(templateId, container, model) {
+  var templateString = $('#' + templateId).text();
+  var templateFunction = _.template(templateString);
+  var renderedTemplate = templateFunction(model);
+  $(container).append(renderedTemplate);
+}
+
+//Get function on loadup
+$(function() {
+
+  $.ajax(url, {
+    type: 'get',
+    dataType: 'json',
+    success: function(data) {
+      $.each(data, function(i, each) {
+        renderTemplate('text-body', '.chirp-holder', each);
+    });
+    }
+  });
+});
+
+
+//Post Function
+$('.button').on('click', function() {
+  $.ajax(url, {
+    type: 'post',
+    dataType: 'json',
+    data: {
+      text: $('#chirp-box').val()
+    },
+    success: function(newData) {
+      renderTemplate('text-body', '.chirp-holder', newData);
+    }
+  });
+});
