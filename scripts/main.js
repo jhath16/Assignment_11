9e08f208124b8b061160f9e1080c301b7a3fd5eb
jhath16 <hathaway.justin@gmail.com>
2014-09-16 14:23:13 -0400

    delete function fixed

---
+++ scripts/main.js
@@ -0,0 +1,52 @@
+var url = "http://tiny-pizza-server.herokuapp.com/collections/chirp";
+
+function renderTemplate(templateId, container, model) {
+  var templateString = $('#' + templateId).text();
+  var templateFunction = _.template(templateString);
+  var renderedTemplate = templateFunction(model);
+  $(container).append(renderedTemplate);
+}
+
+//Get function on loadup
+$(function() {
+
+  $.ajax(url, {
+    type: 'get',
+    dataType: 'json',
+    success: function(data) {
+      $.each(data, function(i, each) {
+        renderTemplate('text-body', '.chirp-holder', each);
+      });
+    }
+  });
+
+
+
+  //Post Function
+  $('.button').on('click', function() {
+    $.ajax(url, {
+      type: 'post',
+      dataType: 'json',
+      data: {
+        text: $('#chirp-box').val()
+      },
+      success: function(newData) {
+        renderTemplate('text-body', '.chirp-holder', newData);
+      }
+    });
+  });
+});
+
+
+//Delete Function
+
+$('#delete').on('click', function() {
+
+  $.ajax(url).done(function(item) {
+    $.ajax({
+      type:'delete',
+      dataType:'json',
+      url:"http://tiny-pizza-server.herokuapp.com/collections/chirp/" + item._id,
+    });
+  });
+});
